8299f7a6c26a67323d41d7485db58c909acec897
Paulo Roberto <paulonill@gmail.com>
2020-04-11 14:19:56 -0300

    Teste WAR

--- src/main/java/br/com/pjr/exemplospringbootthymeleaf/ExemploSpringBootThymeleafApplication.java
+++ src/main/java/br/com/pjr/exemplospringbootthymeleaf/ExemploSpringBootThymeleafApplication.java
@@ -4,12 +4,13 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
-public class ExemploSpringBootThymeleafApplication {
+public class ExemploSpringBootThymeleafApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExemploSpringBootThymeleafApplication.class, args);
@@ -20,3 +21,4 @@ public class ExemploSpringBootThymeleafApplication {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 }
+
